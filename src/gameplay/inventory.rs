debf48a0bd67cf57a936d5e1a4b2a11b759e4c62
Daniel Gallups <dsgallups@protonmail.com>
2026-02-12 03:39:15 -0500

    wip: tommy gun effect

--- src/gameplay/inventory.rs
+++ src/gameplay/inventory.rs
@@ -260,23 +260,71 @@ impl FromWorld for MuzzleFlashEffect {
         size_curve.add_key(0.0, Vec3::splat(0.06));
         size_curve.add_key(1.0, Vec3::splat(0.01));
 
-        let effect = EffectAsset::new(128, SpawnerSettings::once(10.0.into()), module)
-            .with_name("MuzzleFlash")
-            .with_alpha_mode(bevy_hanabi::AlphaMode::Add)
-            .init(init_pos)
-            .init(init_vel)
-            .init(lifetime)
-            .render(ColorOverLifetimeModifier {
-                gradient,
-                ..default()
-            })
-            .render(SizeOverLifetimeModifier {
-                gradient: size_curve,
-                screen_space_size: false,
-            });
+        // let effect = EffectAsset::new(128, SpawnerSettings::once(10.0.into()), module)
+        //     .with_name("MuzzleFlash")
+        //     .with_alpha_mode(bevy_hanabi::AlphaMode::Add)
+        //     .init(init_pos)
+        //     .init(init_vel)
+        //     .init(lifetime)
+        //     .render(ColorOverLifetimeModifier {
+        //         gradient,
+        //         ..default()
+        //     })
+        //     .render(SizeOverLifetimeModifier {
+        //         gradient: size_curve,
+        //         screen_space_size: false,
+        //     });
 
-        Self(effects.add(effect))
+        // Self(effects.add(effect))
+        Self(effects.add(fire_effect()))
     }
+}
+
+pub fn fire_effect() -> EffectAsset {
+    let mut color_gradient1 = HanabiGradient::new();
+    color_gradient1.add_key(0.0, Vec4::splat(1.0));
+    color_gradient1.add_key(0.1, Vec4::new(1.0, 1.0, 0.0, 1.0));
+    color_gradient1.add_key(0.4, Vec4::new(1.0, 0.0, 0.0, 1.0));
+    color_gradient1.add_key(1.0, Vec4::splat(0.0));
+    let mut size_gradient1 = HanabiGradient::new();
+    size_gradient1.add_key(0.0, Vec3::splat(0.1));
+    size_gradient1.add_key(0.5, Vec3::splat(0.5));
+    size_gradient1.add_key(0.8, Vec3::splat(0.08));
+    size_gradient1.add_key(1.0, Vec3::splat(0.0));
+
+    let writer1 = ExprWriter::new();
+
+    let age1 = writer1.lit(0.).expr();
+    let init_age1 = SetAttributeModifier::new(Attribute::AGE, age1);
+
+    let lifetime1 = writer1.lit(5.).expr();
+    let init_lifetime1 = SetAttributeModifier::new(Attribute::LIFETIME, lifetime1);
+
+    // Add constant downward acceleration to simulate gravity
+    let accel1 = writer1.lit(-Vec3::Y * 3.).expr();
+    let update_accel1 = AccelModifier::new(accel1);
+
+    let init_pos1 = SetPositionCone3dModifier {
+        base_radius: writer1.lit(1.).expr(),
+        top_radius: writer1.lit(1.).expr(),
+        height: writer1.lit(0.5).expr(),
+        dimension: ShapeDimension::Surface,
+    };
+
+    EffectAsset::new(32768, SpawnerSettings::rate(100.0.into()), writer1.finish())
+        .with_name("emit:rate")
+        .init(init_pos1)
+        // Make spawned particles move away from the emitter origin
+        //.init(init_vel1)
+        .init(init_age1)
+        .init(init_lifetime1)
+        .update(update_accel1)
+        .render(ColorOverLifetimeModifier::new(color_gradient1))
+        .render(SizeOverLifetimeModifier {
+            gradient: size_gradient1,
+            screen_space_size: false,
+        })
+        .render(OrientModifier::new(OrientMode::FaceCameraPosition))
 }
 
 fn use_tool(
